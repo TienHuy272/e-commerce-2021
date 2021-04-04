96f023e948aa05cde4124e4a9862aea382cc71e3
Nguyen Tien Huy <tienhuy@Nguyens-MacBook-Pro.local>
2021-04-04 18:16:14 +0700

    fix bug

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -14,14 +14,16 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
 export const removeItemFromCart = (cartItems, cartItemToRemove) => {
   const existingCartItem = cartItems.find(
-    (cartItem) => (cartItem.id = cartItemToRemove.id),
+    (cartItem) => cartItem.id === cartItemToRemove.id,
   );
+
   if (existingCartItem.quantity === 1) {
     return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
   }
-  return cartItems.map((item) =>
-    item.id === cartItemToRemove.id
-      ? { ...item, quantity: item.quantity - 1 }
-      : item,
+
+  return cartItems.map((cartItem) =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem,
   );
 };
