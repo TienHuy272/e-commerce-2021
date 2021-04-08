66c5b56623fa9e10b5cec08ab7b2a286fdabac3d
Nguyen Tien Huy <tienhuy@Nguyens-MacBook-Pro.local>
2021-04-08 21:08:21 +0700

    edit NODE_ENV

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,7 @@ import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 
 const middlewares = [];
-if (process.env.NODE_ENV === 'developent') {
+if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger);
 }
 
